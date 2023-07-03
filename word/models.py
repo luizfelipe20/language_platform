7a30c83b5038aff9070184cef81557e925e046ad
luiz.felipe <felipekjs36@gmail.com>
2023-07-03 08:48:20 -0300

    model adjustments

--- word/models.py
+++ word/models.py
@@ -10,10 +10,13 @@ class TypePartSpeechChoices(models.TextChoices):
 class Tags(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     term = models.TextField()
-    language = models.CharField(max_length=50, choices=TypePartSpeechChoices.choices)
+    language = models.CharField(max_length=50, choices=TypePartSpeechChoices.choices, default=TypePartSpeechChoices.ENGLISH)
     tags = models.ManyToManyField('self', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [('term',)]
 
     def __str__(self):
         return f'{self.term}'
@@ -24,12 +27,14 @@ class Terms(models.Model):
     text = models.TextField()
     tags = models.ManyToManyField(Tags, related_name='word_tags', null=True, blank=True)
     pronunciation = models.FileField(upload_to="./audios", null=True, blank=True)
-    # translations = models.ManyToManyField(Translation, related_name='word_translations', null=True, blank=True)
     reference = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     gpt_identifier = models.CharField(max_length=100, null=True, blank=True)
     obs = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [('text',)]
 
     def __str__(self):
         return f'{self.text} - {self.id}'
