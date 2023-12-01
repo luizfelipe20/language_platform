9af571818419416d2c30a38a6aca9e46afac261b
Luiz Felipe <felipekjs36@gmail.com>
2023-12-01 15:42:28 -0300

    adding constraint to model

--- word/models.py
+++ word/models.py
@@ -47,5 +47,8 @@ class Translation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = [('term', 'reference', 'language')]
+
     def __str__(self):
         return f'{self.term} - {self.id}'
