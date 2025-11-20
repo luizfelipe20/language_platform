c3a4f6058c62b7c43713618c6d4c5cb81f22a12d
Luiz Felipe <felipekjs36@gmail.com>
2025-11-20 17:11:41 -0300

    ajustes deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-# Render build script
+# Exit on error
+set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
