22ff870a70befd446ac0d46535d3c3ae026b3c83
Luiz Felipe <felipekjs36@gmail.com>
2025-11-22 15:58:38 -0300

    configuração para amazenamento no s3

--- language_platform/settings.py
+++ language_platform/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'rest_framework',
     'rangefilter',
     'memorization',
@@ -143,5 +144,15 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-LOGIN_REDIRECT_URL = '/vocabulary_test/'
-LOGOUT_REDIRECT_URL = '/admin'
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+
+AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
+
+MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/"
